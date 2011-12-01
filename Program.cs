18a92bb8d440f329b6329e2d2b64066a58ed3ce9
TUPUNCO <tupunco@gmail.com>
2011-12-01 15:09:09 +0800

    commit all

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Tup.SimTrie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var trieTree = new TrieTree();
+            //var lines = File.ReadAllLines("testdata.txt");
+            //trieTree.Build(lines);
+            //bool res = false;
+            //foreach (var item in lines)
+            //{
+            //    res = trieTree.Find(item);
+            //    if (!res)
+            //        throw new ArgumentNullException(item);
+
+            //    res = trieTree.Find(item, true);
+            //    if (!res)
+            //        throw new ArgumentNullException(item);
+            //}
+
+            var trieTree = new TrieTree();
+            trieTree.Build(new string[]{"tupunco","tup","nihao","nihaihaoma","xiaobai","小良"
+                                        });
+            Console.WriteLine("{0}", trieTree.Find("tupunco"));
+            Console.WriteLine("{0}", trieTree.Find("tup"));
+            Console.WriteLine("{0}", trieTree.Find("nihao"));
+            Console.WriteLine("{0}", trieTree.Find("nihaihaoma"));
+            Console.WriteLine("{0}", trieTree.Find("小良"));
+            Console.WriteLine("{0}", trieTree.Find("xiaobai"));
+            Console.WriteLine("----");
+            Console.WriteLine("{0}", trieTree.Find("tux"));
+            Console.WriteLine("{0}", trieTree.Find("helloworld"));
+            Console.WriteLine("{0}", trieTree.Find("hao"));
+            Console.WriteLine("{0}", trieTree.Find("bai"));
+            Console.WriteLine("{0}", trieTree.Find("haihaoma"));
+            Console.WriteLine("{0}", trieTree.Find("unco"));
+            Console.WriteLine("{0}", trieTree);
+            Console.WriteLine("--f1--");
+            Console.WriteLine("{0}", trieTree.Find("tupunco", true));
+            Console.WriteLine("{0}", trieTree.Find("tup", true));
+            Console.WriteLine("{0}", trieTree.Find("nihao", true));
+            Console.WriteLine("{0}", trieTree.Find("nihaihaoma", true));
+            Console.WriteLine("--f2--");
+            Console.WriteLine("{0}", trieTree.Find("tupunc", true));
+            Console.WriteLine("{0}", trieTree.Find("tupuncop", true));
+            Console.WriteLine("{0}", trieTree.Find("tupu", true));
+            Console.WriteLine("{0}", trieTree.Find("nihaom", true));
+            Console.WriteLine("{0}", trieTree.Find("nihaihaomaya", true));
+            Console.WriteLine("--f2--");
+            Console.WriteLine("{0}", trieTree.Find("tupunc"));
+            Console.WriteLine("{0}", trieTree.Find("tupuncom"));
+            Console.WriteLine("{0}", trieTree.Find("tupu"));
+            Console.WriteLine("{0}", trieTree.Find("nihaom"));
+            Console.WriteLine("{0}", trieTree.Find("nihaihaomaya"));
+            Console.Read();
+        }
+    }
+}
---
+++ TrieTree.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tup.SimTrie
+{
+    /// <summary>
+    /// Trie 查找树
+    /// </summary>
+    /// <remarks>
+    /// 参考: http://en.wikipedia.org/wiki/Trie
+    /// </remarks>
+    public class TrieTree
+    {
+        private TrieNode m_root = new TrieNode();
+
+        /// <summary>
+        /// 构建 Trie 树
+        /// </summary>
+        /// <param name="values">参与构建 Trie 树的字符串列表</param>
+        public void Build(IEnumerable<string> values)
+        {
+            if (values == null)
+                throw new ArgumentNullException("values");
+
+            foreach (var item in values)
+            {
+                Build(item);
+            }
+        }
+        /// <summary>
+        /// 构建 Trie 树
+        /// </summary>
+        /// <param name="value">参与构建 Trie 树的字符串</param>
+        public void Build(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException("value");
+
+            TrieNode nNode = null;
+            TrieNode cNode = m_root;
+            var cChildNodes = cNode.ChildNodes;
+            foreach (var curItem in value)
+            {
+                if (cChildNodes == null)
+                    cChildNodes = new Dictionary<char, TrieNode>();
+
+                if (cChildNodes.Count == 0 || !cChildNodes.TryGetValue(curItem, out nNode))
+                {
+                    nNode = new TrieNode();
+                    cChildNodes.Add(curItem, nNode);
+                }
+                cNode.ChildNodes = cChildNodes;
+
+                cNode = nNode;
+                cChildNodes = cNode.ChildNodes;
+            }
+            cNode.IsSegment = true;
+        }
+        /// <summary>
+        /// 判断字符串是否在 Trie 树上
+        /// </summary>
+        /// <param name="value">待匹配字符串</param>
+        /// <returns>找到结果返回True</returns>
+        public bool Find(string value)
+        {
+            return Find(value, false);
+        }
+        /// <summary>
+        /// 判断字符串是否在 Trie 树上
+        /// </summary>
+        /// <param name="value">待匹配字符串</param>
+        /// <param name="fuzzy">是否模糊匹配</param>
+        /// <returns>找到结果返回 True, 模糊匹配传入 True 后, 只要待匹配字符串前缀有匹配情况就返回 True</returns>
+        public bool Find(string value, bool fuzzy)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException("value");
+
+            TrieNode cNode = m_root;
+            var cChildNodes = cNode.ChildNodes;
+            foreach (var curItem in value)
+            {
+                if (cChildNodes != null && cChildNodes.TryGetValue(curItem, out cNode))
+                    cChildNodes = cNode.ChildNodes;
+                else
+                    return false;
+
+                if (fuzzy && cNode.IsSegment)
+                    return true;
+            }
+            return cNode.IsSegment;
+        }
+        /// <summary>
+        /// Trie 节点
+        /// </summary>
+        class TrieNode
+        {
+            /// <summary>
+            /// 子节点
+            /// </summary>
+            public Dictionary<char, TrieNode> ChildNodes { get; set; }
+            /// <summary>
+            /// 当前节点是否是段结束节点
+            /// </summary>
+            public bool IsSegment { get; set; }
+        }
+    }
+}
